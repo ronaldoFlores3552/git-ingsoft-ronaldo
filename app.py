c9c7af4efa2d67bab4c6b8b2b74845a3d1d09f76
Rodo Vilcarromero <rodo.vilcarromero@utec.edu.pe>
2023-08-17 20:32:30 -0500

    Update Branch Rodo

--- app.py
+++ app.py
@@ -15,3 +15,14 @@ def hello():
  
 
     return '<h1>Hello, World editado desde Vscode in branch Rodo!</h1>'
+
+@app.route('/suma')
+def suma(term1=10, term2=20):
+    return f'<h1>f{term1 + term2}<h1>'
+
+@app.route('/multi')
+def multiplicacion(term1=10, term2=20):
+    return f'<h1>f{term1 * term2}<h1>'
+
+if __name__ == '__main__':
+    app.run()
