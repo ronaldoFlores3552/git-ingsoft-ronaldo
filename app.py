20b21210d3979e12dea2b3e741f74932758379af
ronaldoFlores3552 <ronaldo.flores@utec.edu.pe>
2023-08-17 20:17:38 -0500

    Agregando la funcion suma para sumita

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello():
-
- 
-
     return '<h1>Hello, World Ronaldo uwu!</h1>'
+
+@app.route('/sumita')
+def suma(term1=10,term2=20):
+    result=term1+term2
+    return '<h1>El resultado (por defecto) de la funcion suma es: f{result}</h1>'
